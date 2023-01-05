2ae932e9c82329258d2aa2317391fc8857abe9b6
miki5ki <53338596+miki5ki@users.noreply.github.com>
2023-01-05 18:02:45 +0900

    ✨ ユーザーのコントローラーを追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/index'
+  get 'users/show'
+  get 'users/destroy'
   get 'answers/create'
   get 'answers/update'
   get 'answers/destroy'

19a068f9eff19624061bb79b7ed13ba06f15e2c5
miki5ki <53338596+miki5ki@users.noreply.github.com>
2023-01-05 18:48:02 +0900

    ✨ ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 Rails.application.routes.draw do
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/index'
-  get 'users/show'
-  get 'users/destroy'
-  get 'answers/create'
-  get 'answers/update'
-  get 'answers/destroy'
-  get 'questions/new'
-  get 'questions/create'
-  get 'questions/edit'
-  get 'questions/update'
-  get 'questions/index'
-  get 'questions/show'
-  get 'questions/destroy'
+  # get 'users/create'
+  # get 'users/edit'
+  # get 'users/update'
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/destroy'
+  # get 'answers/create'
+  # get 'answers/update'
+  # get 'answers/destroy'
+  # get 'questions/new'
+  # get 'questions/create'
+  # get 'questions/edit'
+  # get 'questions/update'
+  # get 'questions/index'
+  # get 'questions/show'
+  # get 'questions/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resoures :questions
+  resoures :answers
 end
